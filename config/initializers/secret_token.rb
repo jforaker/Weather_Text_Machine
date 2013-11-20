a794154455fbf83152f5e4c1eee1666d23591a9a
jforaker <jake@chalkable.com>
2013-11-19 22:31:13 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeatherTextMachine::Application.config.secret_key_base = 'e067aa58d53de499b7e3f508d2e0364ea264713189bd0680eefa1fbcd458124a1d223533e3f16b88ab39a2e071b9c87ddf5ebd517ab3902611e39697c1edb36b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WeatherTextMachine::Application.config.session_store :cookie_store, key: '_Weather_Text_Machine_session'
